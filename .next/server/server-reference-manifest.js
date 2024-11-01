51ff240a3afc86ef32f33f705c5516b3850863ab
Ankit <ankit.va1511@gmail.com>
2024-11-02 02:53:48 +0530

    add new

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3UwxW6bn1TKsPB+C06W6xbfoBHsUDGWc7V/cX9OHwKY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nhIOkvpfPHjZniSTI9bqG4YMXptf555gS+l4+0CvH2k=\"\n}"
